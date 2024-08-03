b7169fa91b3e2bd2f319920c952431fb56a946b4
mikaeilllll <148379575+mikaeilllll@users.noreply.github.com>
2024-08-03 15:49:53 -0400

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,10 +7,10 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'a3ce63b6-c204-45ec-ad58-8fe8ab32ffc3';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['3.122.86.99'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
